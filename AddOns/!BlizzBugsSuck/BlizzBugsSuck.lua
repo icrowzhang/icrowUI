ab7e8b88b5763a7bb049dbe6d03036e81a4c2d46
icrow <icrowzhang@gmail.com>
2012-10-19 12:14:39 +0800

    去掉冗余内容, 添加防止WorldMapBlobFrame发生taint的代码

--- AddOns/!BlizzBugsSuck/BlizzBugsSuck.lua
+++ AddOns/!BlizzBugsSuck/BlizzBugsSuck.lua
@@ -9,6 +9,8 @@ if GetLocale() == "deDE" then
 	DAY_ONELETTER_ABBR = "%d d"
 end
 
+local Dummy = function() end
+
 -- fixes the issue with InterfaceOptionsFrame_OpenToCategory not actually opening the Category (and not even scrolling to it)
 -- Confirmed still broken in Mists of Pandaria as of build 16016 (5.0.4) 
 do
@@ -71,102 +73,6 @@ do
 	hooksecurefunc("InterfaceOptionsFrame_OpenToCategory", InterfaceOptionsFrame_OpenToCategory_Fix)
 end
 
--- Fix for errors when opening the map that people started finding with
--- 4.0.3a.  The problem exists because QuestPOIGetQuestIDByVisible does
--- not sort completed quests to the top.  If there is an uncompleted
--- quest then Blizzard's implementation of the POI icons breaks since it
--- assumes that all the buttons from 1 to the last known index exists.
---
--- This fix makes the quests sort in the order that the rest of Blizzard's
--- code assumes they will.
---
--- Confirmed fixed in 5.0.4 aka build 16016
-if wow_build < 16016 then
-	local QuestMapUpdateAllQuests_blizz = QuestMapUpdateAllQuests
-	local QuestPOIGetQuestIDByVisibleIndex_blizz = QuestPOIGetQuestIDByVisibleIndex
-	local cache = {}
-
-	local function sort_by_completed(a, b)
-		if not a then
-			return false
-		elseif not b then
-			return true
-		end
-
-		-- completed first
-		local a_completed, b_completed = a.isComplete, b.isComplete
-		if a_completed and not b_completed then
-			return true
-		elseif not a_completed and b_completed then
-			return false
-		end
-
-		-- keep original order otherwise
-		local a_id, b_id = a.i, b.i
-		if not a_id then 
-			return false
-		elseif not b_id then
-			return true
-		end
-		return a_id < b_id
-	end
-
-	function QuestMapUpdateAllQuests()
-		for i=1,#cache do
-			if cache[i] then
-				wipe(cache[i])
-			end
-		end
-		local playerMoney = GetMoney();
-		local numEntries = QuestMapUpdateAllQuests_blizz()
-		for i=1,numEntries do
-			local entry = cache[i]
-			if not entry then
-				entry = {}
-				cache[i] = entry
-			end
-			local questId, questLogIndex = QuestPOIGetQuestIDByVisibleIndex_blizz(i)
-			local _, _, _, _, _, _, isComplete = GetQuestLogTitle(questLogIndex)
-			local requiredMoney = GetQuestLogRequiredMoney(questLogIndex)
-			local numObjectives = GetNumQuestLeaderBoards(questLogIndex)
-			if isComplete and isComplete < 0 then
-				isComplete = false
-			elseif numObjectives == 0 and playerMoney >= requiredMoney then
-				isComplete = true
-			end
-			entry.i = i
-			entry.questId = questId
-			entry.questLogIndex = questLogIndex
-			entry.isComplete = isComplete
-		end 
-		sort(cache, sort_by_completed)
-		return numEntries
-	end
-
-	function QuestPOIGetQuestIDByVisibleIndex(i)
-		local entry = cache[i]
-		if not entry then return nil,nil end
-		return entry.questId, entry.questLogIndex
-	end
-
-end
-
-
--- Fix for whisper menu options and chat links for cross-realm players in LFR and BGs
--- Name parsing currently fails when server names contain spaces, so re-issue them without spaces
--- Confirmed fixed in 5.0.4 aka build 16016
-if wow_build < 16016 then
-	hooksecurefunc("ChatFrame_SendTell", 
-	function(name,...) 
-		if name:find("%s") then 
-			if ACTIVE_CHAT_EDIT_BOX then 
-				ChatEdit_OnHide(ACTIVE_CHAT_EDIT_BOX) 
-			end; 
-			ChatFrame_SendTell(name:gsub("%s",""),...) 
-		end 
-	end)
-end
-
 -- Fix an issue where the GlyphUI depends on the TalentUI but doesn't
 -- always load it.  This issue will manafest with an error like this:
 -- attempt to index global "PlayerTalentFrame" (a nil value)
@@ -185,10 +91,57 @@ if wow_build >= 16016 then
 	frame:RegisterEvent("ADDON_LOADED")
 end
 
--- need to be opened at least one time before logging in, or big chance of taint later ...
-local taint = CreateFrame("Frame")
-taint:RegisterEvent("PLAYER_ENTERING_WORLD")
-taint:SetScript("OnEvent", function(self, event, addon)
-	ToggleFrame(SpellBookFrame)
-	ToggleFrame(SpellBookFrame)
+-- code from Grimsin @ wowinterface.com, to fix the WorldMapBlobFrame taint issue
+local GUIWorldMapEvents = CreateFrame("Frame", nil, UIParent)
+GUIWorldMapEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+GUIWorldMapEvents:RegisterEvent("PLAYER_REGEN_ENABLED") 
+GUIWorldMapEvents:RegisterEvent("PLAYER_REGEN_DISABLED")
+ 
+GUIWorldMapEvents:SetScript("OnEvent", function(self)
+    if event == "PLAYER_ENTERING_WORLD" then
+        ToggleFrame(WorldMapFrame)
+        ToggleFrame(WorldMapFrame)
+		ToggleFrame(SpellBookFrame)
+		ToggleFrame(SpellBookFrame)
+    
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        WatchFrame.showObjectives = nil;
+        WorldMapQuestShowObjectives:SetChecked(false)
+        WorldMapQuestShowObjectives:Hide()
+        WorldMapTrackQuest:Hide()
+        WorldMapTitleButton:Hide()
+        WorldMapFrameSizeUpButton:Hide()
+        WorldMapBlobFrame:Hide()
+        WorldMapPOIFrame:Hide()
+        
+        WorldMapFrameSizeUpButton.Show = Dummy
+        WorldMapQuestShowObjectives.Show = Dummy
+        WorldMapTrackQuest.Show = Dummy
+        WorldMapTitleButton.Show = Dummy
+        WorldMapBlobFrame.Show = Dummy
+        WorldMapPOIFrame.Show = Dummy
+        
+        WatchFrame_Update();
+    
+    elseif event == "PLAYER_REGEN_ENABLED" then
+        
+        WorldMapFrameSizeUpButton.Show = WorldMapFrameSizeUpButton:Show()
+        WorldMapQuestShowObjectives.Show = WorldMapQuestShowObjectives:Show()
+        WorldMapTrackQuest.Show = WorldMapTrackQuest:Show()
+        WorldMapTitleButton.Show = WorldMapTitleButton:Show()
+        WorldMapBlobFrame.Show = WorldMapBlobFrame:Show()
+        WorldMapPOIFrame.Show = WorldMapPOIFrame:Show()
+        
+        WatchFrame.showObjectives = true;
+        WorldMapQuestShowObjectives:SetChecked(true)
+ 
+        WorldMapQuestShowObjectives:Show()
+        WorldMapTrackQuest:Show()
+        WorldMapTitleButton:Show()
+        WorldMapFrameSizeUpButton:Show()
+        WorldMapBlobFrame:Show()
+        WorldMapPOIFrame:Show()
+        
+        WatchFrame_Update();
+    end
 end)
